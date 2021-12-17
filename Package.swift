e35aae7b0628e5805606fe922616a47b88111294
Julius Naeumann <julius.naeumann@gmail.com>
2021-12-17 12:20:21 +0100

    created swift package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
-	name: "SavannaKit"
+	name: "SavannaKit",
+    platforms: [
+        .iOS(.v13)],
+    products: [
+        .library(name: "SavannaKit", type: nil, targets: ["SavannaKit"])],
+    targets: [
+        .target(name: "SavannaKit")
+    ]
+    
 )
